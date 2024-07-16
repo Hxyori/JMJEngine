2935a785854d7f7be67f4115cc597d31e9840827
Jamie <54281096+Hxyori@users.noreply.github.com>
2024-07-16 10:36:57 +0200

    .

--- main.js
+++ main.js
@@ -1,10 +1,24 @@
+//# 1€ start
+// 0.25€ pro min
+// 1.50 pro km
 function calculateResult() {
-    var duration;
-    var route;
-    var result;
+    var duration = document.getElementById('duration');
+    var route = document.getElementById('route');
+    var cost = 0.25;
+    var calcOption = document.getElementById('calc_options');
+    var routeOption = document.getElementById('route_option');
+    var discountCode = document.getElementById('discountCode');
+
+    if (routeOption = "km") {
+        route = route * 1000; 
+    }
+
     
-    route = document.getElementById('route');
-    duration = document.getElementById('duration');
+
+    if (discountCode.length > 0) {
+        //Todo
+    }
+
 
     //ToDO 
 }
